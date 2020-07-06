fc9cd44f46bb9674498699dd4c1db809409d05a6
AwrenNuit <a.rennaker@yahoo.com>
2020-07-05 20:06:42 -0500

    set up basic grid and array map

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HerbList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HerbList from "./components/HerbList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HerbList,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/HerbList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main-grid">
+    <div v-for="herb in herbs" :key="herb">
+      <h2>{{ herb.name }}</h2>
+      <p>{{ herb.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      herbs: [
+        { name: "Goldenrod", description: "Goldenrod is the best!" },
+        { name: "Yarrow", description: "Yarrow is the best!" },
+        { name: "Plantain", description: "Plantian is the best!" },
+        { name: "Elderberry", description: "Elderberry is the best!" },
+        { name: "Mugwort", description: "Mugwort is the best!" },
+        { name: "Bee Balm", description: "Bee Balm is the best!" },
+      ],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main-grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+}
+</style>
