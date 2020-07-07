5c07337692d90bd99c2e5895a5695294299ed6b7
AwrenNuit <a.rennaker@yahoo.com>
2020-07-07 14:26:59 -0500

    set up data store, imported stock images

--- src/components/SelectedHerb.vue
+++ src/components/SelectedHerb.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
+    <img :src="herb.image" alt="" height="300" width="300"/>
     <h2>{{ herb.name }}</h2>
-    <p>Other Names: {{ herb.otherNames }}</p>
-    <p>Description: {{ herb.description }}</p>
-    <p>Found In: {{ herb.location }}</p>
-    <p>Uses: {{ herb.uses }}</p>
+    <div id="content-container">
+      <p>Other Names: {{ herb.otherNames }}</p>
+      <p>Description: {{ herb.description }}</p>
+      <p>Found In: {{ herb.location }}</p>
+      <p>Uses: {{ herb.uses }}</p>
+    </div>
   </div>
 </template>
 
@@ -15,7 +18,22 @@ export default {
       herb: this.$store.state.selectedHerb,
     };
   },
+  mounted() {
+    if (!this.$store.state.selectedHerb.name) {
+      this.$router.push("/");
+    }
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+img {
+  object-fit: cover;
+}
+
+#content-container {
+  margin: 0 auto;
+  text-align: left;
+  width: 500px;
+}
+</style>
